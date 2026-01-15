326562b26f58d38028b35d0e3759b45aaa9d157f
EnriqueNieto90 <enrikeniet90@gmail.com>
2026-01-15 09:55:26 +0100

    Primer commit

---
+++ indexProyectoCIB.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>CFGS - Desarrollo de Aplicaciones Web</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background: #f4f6f9;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background: green;
+            color: white;
+            padding: 15px;
+            text-align: center;
+        }
+        h1 {
+            margin: 0;
+        }
+        main {
+            max-width: 800px;
+            margin: 30px auto;
+            padding: 20px;
+            background: white;
+            border-radius: 10px;
+            box-shadow: 0 4px 10px rgba(0,0,0,0.1);
+        }
+        ul {
+            list-style: none;
+            padding: 0;
+        }
+        li {
+            background: #ecf0f1;
+            margin: 10px 0;
+            padding: 15px;
+            border-left: 5px solid green;
+            border-right: 5px solid green;
+            transition: 0.3s;
+	    border-radius:8px;
+        }
+        li:hover {
+            background: #d6eaf8;
+            border-left: 5px solid purple;
+            border-right: 5px solid purple;
+        }
+
+        footer{
+            margin: auto;
+            background-color: green;
+            text-align: center;
+            height: 150px;
+	    color: white;
+        }
+	main{
+	text-align:center;
+	justify-content:center;
+	}
+
+    </style>
+</head>
+<body>
+    <header>
+        <h2>CFGS - Desarrollo de Aplicaciones Web</h2>
+        <h1><b>Enrique Nieto Lorenzo</b></h1>
+        <p>Curso 2025/2026 - Grupo DAW2 - IES Los Sauces</p>
+    </header>
+    <main>
+        <h2><b>FUNDAMENTOS DE CIBERSEGURIDAD</b></h2>
+        <ul>
+            <li><a href="doc/GlosarioTerminos.pdf" target="_blank">GLOSARIO DE TÉRMINOS</a></li>
+        </ul>
+        <ul>
+            <li><a href="doc/README.html" target="_blank">INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</a></li>
+        </ul>
+        <ul>
+            <li><a href="doc/251103ProtocoloInstalacionServidorWebAMP.pdf" target="_blank">PROTOCOLO DE INSTALACIÓN</a></li>
+        </ul>
+    </main>
+
+    <footer>
+        <caption>
+            <a href="/">Enrique Nieto Lorenzo</a> | 03/10/2025
+        </caption>
+    </footer>
+</body>
+</html>
